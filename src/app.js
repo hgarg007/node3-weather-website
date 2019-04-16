7d7b09d1d380b9459d8bfbd14dc89b29fcbd3509
hgarg <himanshugarg007@hotmail.com>
2019-04-16 06:05:10 -0400

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 const publicDirPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -71,7 +73,7 @@ app.get('*', (req, res) => {
     res.send('My 404 page.')
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up!')
 })
 
